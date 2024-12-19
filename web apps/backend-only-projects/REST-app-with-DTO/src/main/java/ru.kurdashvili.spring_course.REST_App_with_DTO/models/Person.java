b1826f05123d0677c73c4f09a6db7a3f47d269de
Irakliy Kurdashvili <71609083+irakliy0701@users.noreply.github.com>
2024-12-19 22:48:29 +0300

    Add files via upload

---
+++ web apps/backend-only-projects/REST-app-with-DTO/src/main/java/ru.kurdashvili.spring_course.REST_App_with_DTO/models/Person.java
@@ -0,0 +1,112 @@
+package ru.kurdashvili.spring_course.REST_App_with_DTO.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "PersonDTO")
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 chars")
+    private String name;
+
+    @Column(name = "age")
+    @Min(value = 1, message = "Age should be greater than 0")
+    private int age;
+
+    @Column(name = "email")
+    @Email
+    @NotEmpty(message = "Email should not be empty")
+    private String email;
+
+    // Следующие внизу 3 поля, должны назначаться на сервере (клиент их
+    // не присылает) -> теперь есть разница между тем объектом, с помощью
+    // которого мы общаемся с клиентом, и объектом, который используем в кач-ве модели
+    // -> поэтому создадим отдельный класс DTO, который будет служить для общения
+    // с клиент
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "created_who")
+    @NotEmpty
+    private String createdWho;
+
+    // Констурктор по умолчанию нужен для Spring
+    public Person(){}
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Обязательно нужны геттеры и сеттеры для Jackson в частности
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getCreatedWho() {
+        return createdWho;
+    }
+
+    public void setCreatedWho(String createdWho) {
+        this.createdWho = createdWho;
+    }
+}
